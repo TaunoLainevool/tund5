2305991851c3c3a275698525f3d23ddd8bc61f69
Tauno Lainevool <taunlai@greeny.cs.tlu.ee>
2015-10-02 10:51:36 +0300

    functions1

--- functions.php
+++ functions.php
@@ -1,1 +1,22 @@
+ <?php
+ 
+ 
+ 
+ 
+ function welcomeUser($name, $age){
+	 echo "Tere " .$name.", kelle vanus on " .$age."<br>";
+	 
+	 
+ }
+	$first_name="Juku";
+	welcomeUser($first_name, 5);
+
+	$first_name="Juhan";
+	welcomeUser($first_name, 5);
+	
+	welcomeUser ("Mihkel", 11);
+	
+	
+	
+?>
  
